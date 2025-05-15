af07da1ba738069e498592e59fd31d29fe2a93fa
Rachkid <abouzeidirachid97@gmail.com>
2025-05-15 03:54:01 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# ce code compte le nombre de fichiers contenu dans un dossier
-#ls -1 : Liste les fichiers 1 par ligne.
+
+# ce code compte le nombre de fichiers contenu dans un dossier.
+#ls : Liste les fichiers 1 par ligne.
 #wc -l : Compte le nombre de fichiers.
 read file
 count=$(ls "$file" | wc -l)
