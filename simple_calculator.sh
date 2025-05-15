96254ea837f156fa8b0855699552e6d49f979ee1
Rachkid <abouzeidirachid97@gmail.com>
2025-05-15 02:47:00 +0100

    Create simple_calculator.sh

---
+++ simple_calculator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ce code permet d'effectuer des opérations mathématiques
+
+a=$1
+b=$2
+op=$3
+resultat=$1 $2 $3
+
+read a b op
+
+if [ "$op" = "+" ]; then
+    resultat=$((a + b))
+elif [ "$op" = "-" ]; then
+    resultat=$((a - b))
+elif [ "$op" = "*" ]; then
+    resultat=$((a * b))
+elif [ "$op" = "/" ]; then
+    resultat=$((a / b))
+fi
+
+echo "le resultat est : $resultat"
+exit 0
