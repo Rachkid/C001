6116ca8fbdfc24879df73464480c3b1314dcfb0a
Rachkid <abouzeidirachid97@gmail.com>
2025-05-15 03:56:07 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -8,7 +8,7 @@ read fichier
 read mot_a_trouve
 
 if grep -q "$mot_a_trouve" "$fichier" ; then
-        echo "La chaine '$mot_a_trouve' a été trouvée dans $fichier"
+        echo "La chaine '$mot_a_trouve' a été trouvée dans $fichier."
 else
-        echo "La chaine '$mot_a_trouve' n'a pas été trouvée dans $fichier"
+        echo "La chaine '$mot_a_trouve' n'a pas été trouvée dans $fichier."
 fi
